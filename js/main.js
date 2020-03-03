c0620bf080d56ad4dfb63cbe0a09fe9b7c0ade71
Lila Bajraktari <bajr0005@algonquinlive.com>
2020-03-03 09:38:30 -0500

    Make final changes

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ var newName = prompt('What is your name?');
 if (newName == 'Thomas') {
   alert('Names are the same!');
 } else {
-  alert('Names are different.')
+  alert('Names are different.');
 }
